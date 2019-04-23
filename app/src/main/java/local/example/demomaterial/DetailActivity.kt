8fe9621ef52b1a19425dde1b1e06344017d39411
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-04-23 08:09:21 +0200

    Create DetailActivity.kt

---
+++ app/src/main/java/local/example/demomaterial/DetailActivity.kt
@@ -0,0 +1,44 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.demomaterial
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val sportsTitle = findViewById<TextView>(R.id.title_detail)
+        val sportsImage = findViewById<ImageView>(R.id.sport_image_detail)
+
+        sportsTitle.text = intent.getStringExtra("title")
+
+        Glide.with(this).load(intent.getIntExtra(
+            "image_resource",
+            0
+        ))
+            .into(sportsImage)
+    }
+}
