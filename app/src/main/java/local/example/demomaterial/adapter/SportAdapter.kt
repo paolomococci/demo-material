ef5c7e53e87b96096940d4f46abb9d66071ee1d2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-04-24 09:38:57 +0200

    Update SportAdapter.kt

--- app/src/main/java/local/example/demomaterial/adapter/SportAdapter.kt
+++ app/src/main/java/local/example/demomaterial/adapter/SportAdapter.kt
@@ -16,7 +16,7 @@
  *
  */
 
-package local.example.demomaterial.adapter
+package local.example.sportnews.adapter
 
 import android.content.Context
 import android.content.Intent
@@ -27,9 +27,9 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import local.example.demomaterial.DetailActivity
-import local.example.demomaterial.R
-import local.example.demomaterial.model.Sport
+import local.example.sportnews.DetailActivity
+import local.example.sportnews.R
+import local.example.sportnews.model.Sport
 
 import java.util.ArrayList
 
